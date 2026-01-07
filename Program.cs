f8d004d217414e963de0eaa8857a6bfb97c73bdb
Yel0wPikmin <nathanael.mone@outlook.fr>
2026-01-07 16:35:14 +0100

    Add IDE and publish profile configuration files
    Added new .idea and Properties/PublishProfiles files for IDE and publishing configuration. Updated .vs and project user files, and made changes to Program.cs. These changes help manage development environment settings and deployment profiles.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.Text;
-string path = @"C:\tmp\PyKube.py";
-string path2 = @"C:\tmp\PyKube.bat";
-bool DoCleanTMPFiles = true;
+
+string AppTmpDir = @"C:\tmp\";
+string PythonFile = AppTmpDir + @"PyKube.py";
+string BatchFile = AppTmpDir + @"PyKube.bat";
+bool DoCleanTMPFiles = false;
 int colorNumber = 2;
 string KubeParts = "#";
 
@@ -26,45 +28,25 @@ List<ConsoleColor> colorsList = new List<ConsoleColor>()
     ConsoleColor.White
 };
 string PyTranslatedColor = (@"'" + colorsList[colorNumber] + "'");
+
 void changeColor()
-{   
-    
-    
+{
     var color = colorsList[colorNumber];
-
-
-
-        Console.Clear();
-        colorNumber++;
-        Console.ForegroundColor = color;
-         Console.WriteLine($"Change Color ? \n press enter for next\n Escape to continue \n Color : {color} color number : {colorNumber}");
-        ConsoleKeyInfo key = Console.ReadKey(true);
-        if (colorNumber > 15) { colorNumber = 0; changeColor(); }
+    Console.Clear();
+    colorNumber++;
+    Console.ForegroundColor = color;
+    Console.WriteLine($"Change Color ? \n press enter for next\n Escape to continue \n Color : {color} color number : {colorNumber}");
+    ConsoleKeyInfo key = Console.ReadKey(true);
+    if (colorNumber > 15) { colorNumber = 0; changeColor(); }
     else
     {
         if (key.Key == ConsoleKey.Escape) { KubeBuilder(); }
         else { Console.Clear(); changeColor(); }
     }
-
-
-
-
-
-
-
 }
-void sayIt(string sayIt) { Console.Write(sayIt); }
-void DoChangeColorASK()
-{
-    Console.WriteLine("Chage Kube Color ? Y/N");
-    string userIntuptDoseWantToChnageColor = Console.ReadLine();
-    if (userIntuptDoseWantToChnageColor == "Y") { changeColor(); }
-    else if (userIntuptDoseWantToChnageColor == "y") { Console.Clear(); changeColor(); }
-    else { KubeBuilder(); }
-}
-
-void KubePartsBuilder(){ Console.Write(KubeParts);}
-Console.WriteLine("Enter the size of the cube X :");
+void SayIt(string sayIt) { Console.Write(sayIt); }
+void KubePartsBuilder() { Console.Write(KubeParts); }
+SayIt("Enter the size of the cube X :");
 int X = Convert.ToInt32(Console.ReadLine());
 Console.Clear();
 int staticX = X;
@@ -83,11 +65,11 @@ void KubeBuilder()
     {
         X = staticX * 2;
         KubeXbuilder();
-        X = staticX*2;
+        X = staticX * 2;
         Y--;
         Thread.Sleep(10);
         Console.WriteLine("");
-       
+
     }
     void KubeXbuilder()
     {
@@ -99,30 +81,25 @@ void KubeBuilder()
     }
     Console.WriteLine("\n Press Enter to Generate Final Cube ");
     Console.ReadLine();
-/*    if (!System.IO.Directory.Exists(@"C:\\tmp"))
+    if (!Directory.Exists(AppTmpDir))
     {
-        System.IO.Directory.CreateDirectory(@"C:\\tmp");
-    }*/
-    using (FileStream fs = File.Create(path))
-    {
-        byte[] info = new UTF8Encoding(true).GetBytes($"import turtle\r\n\r\nturtle.title('PyKube')\r\nfrom time import sleep\r\nturtle.color({PyTranslatedColor})\r\nfor i in range(2):\r\n    sleep(0.2)\r\n    turtle.begin_fill()\r\n    turtle.forward({staticX*10})\r\n    turtle.left(90)\r\n    turtle.forward({staticY * 10})\r\n    turtle.left(90)\r\n    sleep(0.2)\r\n    turtle.end_fill()\r\nturtle.penup()\r\nturtle.left(90)\r\nturtle.left(90)\r\nturtle.forward(1000)\r\nturtle.done()\r\n\r\n");
-        fs.Write(info, 0, info.Length);
-        fs.Flush();
-        fs.Close();
-
-      
+        Directory.CreateDirectory(AppTmpDir);
     }
-    using (FileStream fs = File.Create(path2))
-    {
-        byte[] info = new UTF8Encoding(true).GetBytes($"start {path} \n cd C://tmp \n del python-3.14.2-amd64.exe \n del dotnet-runtime-10.0.1-win-x64.exe ");
-        fs.Write(info, 0, info.Length);
-        fs.Flush();
-        fs.Close();
+    var pythonTurtleCode = $"import turtle\r\n\r\nturtle.title('PyKube')\r\nfrom time import sleep\r\nturtle.color({PyTranslatedColor})\r\nfor i in range(2):\r\n    sleep(0.2)\r\n    turtle.begin_fill()\r\n    turtle.forward({staticX * 10})\r\n    turtle.left(90)\r\n    turtle.forward({staticY * 10})\r\n    turtle.left(90)\r\n    sleep(0.2)\r\n    turtle.end_fill()\r\nturtle.penup()\r\nturtle.left(90)\r\nturtle.left(90)\r\nturtle.forward(1000)\r\nturtle.done()\r\n\r\n";
+    WriteFile(PythonFile, pythonTurtleCode);
+    var batchPythonLaunch = $"start {PythonFile}";
+    WriteFile(BatchFile, batchPythonLaunch);
 
-        
-
-    }
     System.Diagnostics.Process.Start(@"C:\tmp\PyKube.bat");
-    if (DoCleanTMPFiles == true) {sayIt("Cleanig TMP files... "); Thread.Sleep(2000); File.Delete(path2); Thread.Sleep(5000); File.Delete(path);}
-    else { Console.Beep(); Console.WriteLine(@"Data stored on C:\tmp"); Thread.Sleep(1000); File.Delete(path2); Console.Read();}
+    if (DoCleanTMPFiles == true) { SayIt("Cleanig TMP files... "); Thread.Sleep(2000); File.Delete(BatchFile); Thread.Sleep(5000); File.Delete(PythonFile); }
+    else { Console.Beep(); Console.WriteLine(@"Data stored on C:\tmp"); Thread.Sleep(1000); File.Delete(BatchFile); Console.Read(); }
+}
+
+static void WriteFile(string path, string content)
+{
+    using FileStream fs = File.Create(path);
+    byte[] info = new UTF8Encoding(true).GetBytes(content);
+    fs.Write(info, 0, info.Length);
+    fs.Flush();
+    fs.Close();
 }
